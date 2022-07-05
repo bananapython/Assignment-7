a7b009bb697a5c348b44daf6af9475360b5ffd80
oneoddoddity <47764326+oneoddoddity@users.noreply.github.com>
2022-07-04 20:51:14 -0500

    Update ExpressionTree.py

--- ExpressionTree.py
+++ ExpressionTree.py
@@ -1,24 +1,26 @@
 #  File: ExpressionTree.py
 
-#  Description:
+#  Description: This program creates an expression tree from a given infix expression, evaluates it, and prints it and the prefix/postfix expressions out.
 
-#  Student Name:
+#  Student Name: Emily Wang
 
-#  Student UT EID:
+#  Student UT EID: ew6985
 
-#  Partner Name:
+#  Partner Name: Sean Thomas
 
-#  Partner UT EID:
+#  Partner UT EID: sft372
 
 #  Course Name: CS 313E
 
-#  Unique Number: 
+#  Unique Number: 86439
 
-#  Date Created:
+#  Date Created: 7/1/2022
 
-#  Date Last Modified:
+#  Date Last Modified: 7/4/2022
+
 
 import sys
+import operator
 
 operators = ['+', '-', '*', '/', '//', '%', '**']
 
@@ -70,16 +72,30 @@ class Tree (object):
             else:
                 self.current.data = tkn
                 self.current = self.nodes.pop()
-            
-                
-                
-                
-            
     
     # this function should evaluate the tree's expression
     # returns the value of the expression after being calculated
-    def evaluate (self, aNode):
-        pass
+    def evaluate (self, aNode): 
+        prefix=self.pre_order(aNode)
+        prefix=prefix.split() #turn from string to mutable type
+        stack=[]
+        op = {"+":operator.add,"-":operator.sub,"*":operator.mul,"/":operator.truediv, \
+               "//":operator.floordiv,"%" : operator.mod,"**" : operator.pow,}
+                #uses lookup dictionary method instead of eval to avoid eval's problems
+
+        while prefix: #converts from prefix to infix while evaluating
+            char=prefix.pop()
+            if char in operators:
+                operand1=float(stack.pop())
+                operand2=float(stack.pop())
+                result=op[char](operand1,operand2)
+                stack.append(result)
+                
+            else:
+                stack.append(char)
+        
+        return stack[0]
+    
     # this function should generate the preorder notation of 
     # the tree's expression
     # returns a string of the expression written in preorder notation
@@ -90,6 +106,7 @@ class Tree (object):
             spacedExpr = ""
             for tkn in tokens:
                 spacedExpr += tkn + " "
+        
             return spacedExpr
         return ""
     
@@ -106,23 +123,25 @@ class Tree (object):
                 spacedExpr += tkn + " "
             return spacedExpr
         return ""
+
 # you should NOT need to touch main, everything should be handled for you
 def main():
     # read infix expression
     line = sys.stdin.readline()
     expr = line.strip()
- 
+    
     tree = Tree()
     tree.create_tree(expr)
     
     # evaluate the expression and print the result
     print(expr, "=", str(tree.evaluate(tree.root)))
-
+    
     # get the prefix version of the expression and print
     print("Prefix Expression:", tree.pre_order(tree.root).strip())
-
+    
     # get the postfix version of the expression and print
     print("Postfix Expression:", tree.post_order(tree.root).strip())
-
+    
 if __name__ == "__main__":
     main()
+
